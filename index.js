10b5b5107dd381075feb19e5f07ab79b6425a2b5
daniellwdb7 <37012984+daniellwdb7@users.noreply.github.com>
2018-03-05 21:52:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
   if(!args[0]) return message.channel.send("no");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+  message.channel.send(`Clear ${args[0]} messages.`));
 });
 
 }
