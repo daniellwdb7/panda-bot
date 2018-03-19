3e4e1b7ea6b8e7ca4d58d2d8444556bb8b3b1b67
daniellwdb7 <37012984+daniellwdb7@users.noreply.github.com>
2018-03-19 15:50:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on('message', function() {
     if (message.content === "$loop") { 
       var interval = setInterval (function () {
         message.channel.send("Ik test of ik wakker kan blijven")
-      }, 10000); 
+      }, 1000); 
     }
 });  
 // End test
