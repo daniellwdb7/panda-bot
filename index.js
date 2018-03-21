f055dc6fce8c26fde7f3a658e053269f3bd2bbe5
daniellwdb7 <37012984+daniellwdb7@users.noreply.github.com>
2018-03-21 23:21:41 +0100

    test

--- index.js
+++ index.js
@@ -6,110 +6,110 @@ bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
 
-if(err) console.log(err);
+	if(err) console.log(err);
 
-let jsfile = files.filter(f => f.split(".").pop() === "js")
-if(jsfile.length <= 0){
-console.log("Geen commando's gevonden");
-return;
-}
+	let jsfile = files.filter(f => f.split(".").pop() === "js")
+	if(jsfile.length <= 0){
+		console.log("Geen commando's gevonden");
+		return;
+	}
 
-jsfile.forEach((f, i) => {
-let props = require(`./commands/${f}`);
-console.log(`${f} geladen!`);
-bot.commands.set(props.help.name, props);
-});
+	jsfile.forEach((f, i) => {
+		let props = require(`./commands/${f}`);
+		console.log(`${f} geladen!`);
+		bot.commands.set(props.help.name, props);
+	});
 });
 
 bot.on("ready", async () => {
-console.log(`${bot.user.username} is online!`);
-bot.user.setActivity("Golden Palace", {type: "WATCHING"});
+	console.log(`${bot.user.username} is online!`);
+	bot.user.setActivity("Golden Palace", {type: "WATCHING"});
 });
 
 bot.on("message", async message => {
-if(message.author.bot) return;
-if(message.channel.type === "dm") return;
+	if(message.author.bot) return;
+	if(message.channel.type === "dm") return;
 
-let prefix = botconfig.prefix;
-let messageArray = message.content.split(" ");
-let cmd = messageArray[0];
-let args = messageArray.slice(1);
+	let prefix = botconfig.prefix;
+	let messageArray = message.content.split(" ");
+	let cmd = messageArray[0];
+	let args = messageArray.slice(1);
 
-let commandfile = bot.commands.get(cmd.slice(prefix.length));
-if(commandfile) commandfile.run(bot,message,args);
+	let commandfile = bot.commands.get(cmd.slice(prefix.length));
+	if(commandfile) commandfile.run(bot,message,args);
 
-module.exports.run = async (bot, message, args) => {
+	module.exports.run = async (bot, message, args) => {
 
-if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-if(!args[0]) return message.channel.send("no");
-message.channel.bulkDelete(args[0]).then(() => {
-message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
-});
+		if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
+		if(!args[0]) return message.channel.send("no");
+		message.channel.bulkDelete(args[0]).then(() => {
+			message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+		});
 
-}
+	}
 
-module.exports.help = {
-name: "clear"
-}
+	module.exports.help = {
+		name: "clear"
+	}
 
-module.exports.help = {
-name: "clear"
-}	
+	module.exports.help = {
+		name: "clear"
+	}	
 
-if(cmd === `${prefix}botinfo`){
+	if(cmd === `${prefix}botinfo`){
 
-let bicon = bot.user.displayAvatarURL;
-let botembed = new Discord.RichEmbed()
-.setDescription("Bot Info")
-.setColor("#5856D6")
-.setThumbnail(bicon)
-.addField("Eigenaar:", "Daniell")
-.addField("Naam:", bot.user.username)
-.addField("Gemaakt op:", bot.user.createdAt);
+		let bicon = bot.user.displayAvatarURL;
+		let botembed = new Discord.RichEmbed()
+		.setDescription("Bot Info")
+		.setColor("#5856D6")
+		.setThumbnail(bicon)
+		.addField("Eigenaar:", "Daniell")
+		.addField("Naam:", bot.user.username)
+		.addField("Gemaakt op:", bot.user.createdAt);
 
-return message.channel.send(botembed);
-}
+		return message.channel.send(botembed);
+	}
 
-if(cmd === `Nick`){
+	if(cmd === `Nick`){
 
-let bicon = bot.user.displayAvatarURL;
-let botembed = new Discord.RichEmbed()
-.setColor("#5856D6")
-.setThumbnail(bicon)
-.addField("Wie is Nick?", "Nick moet een ban krijgen!")
+		let bicon = bot.user.displayAvatarURL;
+		let botembed = new Discord.RichEmbed()
+		.setColor("#5856D6")
+		.setThumbnail(bicon)
+		.addField("Wie is Nick?", "Nick moet een ban krijgen!")
 
-return message.channel.send(botembed);
-}
+		return message.channel.send(botembed);
+	}
 
-if(cmd === `Gido`){
+	if(cmd === `Gido`){
 
-let bicon = bot.user.displayAvatarURL;
-let botembed = new Discord.RichEmbed()
-.setColor("#5856D6")
-.setThumbnail(bicon)
-.addField("Wie is Gido?", "Gido moet een ban krijgen!")
+		let bicon = bot.user.displayAvatarURL;
+		let botembed = new Discord.RichEmbed()
+		.setColor("#5856D6")
+		.setThumbnail(bicon)
+		.addField("Wie is Gido?", "Gido moet een ban krijgen!")
 
-return message.channel.send(botembed);
-}  	    
-  
-if(cmd === `${prefix}commands`){
+		return message.channel.send(botembed);
+	}  	    
+	
+	if(cmd === `${prefix}commands`){
 
-let botembed = new Discord.RichEmbed()
-.setDescription("Beschikbare commando's")
-.setColor("#5856D6")
-.addField("Afwezig melden:", "```!report @gebruiker reden en datum```")
+		let botembed = new Discord.RichEmbed()
+		.setDescription("Beschikbare commando's")
+		.setColor("#5856D6")
+		.addField("Afwezig melden:", "```!report @gebruiker reden en datum```")
 
-return message.channel.send(botembed);
-}	
+		return message.channel.send(botembed);
+	}	
 
-if(cmd === `${prefix}verjaardagen`){
+	if(cmd === `${prefix}verjaardagen`){
 
-let botembed = new Discord.RichEmbed()
-.setColor("#5856D6")
-.addField("Verjaardagen:", "Tibby: 12-05 \n Mitch: 20-01")
+		bot.sendMessage(msg.channel, "Online users:");
+		bot.sendMessage(msg.channel, "user 1");
+		bot.sendMessage(msg.channel, "user 2:");
 
-return message.channel.send(botembed);
-}	
+		return message.channel.send(botembed);
+	}	
 
 });
 
